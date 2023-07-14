1d6218a4d1f07a7e40db7e9b3c86bab02ae81b22
bomboonsan <bomboonsan@gmail.com>
2023-07-14 14:37:58 +0700

    login

---
+++ pages/api/login.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    try {
+  
+      // Make a request to the external API using the dynamic parameter
+      const response = await axios.get(`https://ventbackend.wish-integrate.com/event/all`);
+  
+      // Extract the data from the response
+      const data = response.data;
+  
+      // Return the data as the API response
+      res.status(200).json(data);
+    } catch (error) {
+      console.error('Error fetching external API:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,8 @@ export default function Home() {
     // Fetch user data from API
     const fetchEventPwd = async () => {
       try {
-        const response = await fetch('https://ventbackend.wish-integrate.com/event/all', {
+        // const response = await fetch('https://ventbackend.wish-integrate.com/event/all', {
+          const response = await fetch('/api/login', {
           // headers: {
           //   Authorization: 'Bearer YOUR_ACCESS_TOKEN',
           // },
