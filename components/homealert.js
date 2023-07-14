82803b243e51236f987d1d68d0bc4036b7ac9d57
bomboonsan <bomboonsan@gmail.com>
2023-07-14 14:11:57 +0700

    home

--- components/homealert.js
+++ components/homealert.js
@@ -3,7 +3,7 @@ import styles from './homealert.module.scss'
 export default function HomeAlert() {
     return (
         <div className={styles.alert_box}>
-            <p>Password incorrec</p>
+            <p>Password incorrect</p>
         </div>        
     )
 }
